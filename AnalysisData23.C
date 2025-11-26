7c07650d9f25d139108c99130b551720fdf2721e
SzymonGozdowski <gozdosz@gmail.com>
2025-11-26 17:34:03 +0100

    Add files via upload

--- AnalysisData23.C
+++ AnalysisData23.C
@@ -18,13 +18,14 @@ void AnalysisData23::Loop()
    // Initialise histograms   
    TH1F *histEta = new TH1F("histEta", "histEta", 100, -3, 3); 
    TH1F *histPhi = new TH1F("histPhi", "histPhi", 100, -3, 3); 
-   TH1F *histPt = new TH1F("histPt", "histPt", 100, 0.1, 5); // na razie gorna granica z dupy 
-   TH1F *histMass = new TH1F("histMass", "histMass", 100, 0, 10);
+   TH1F *histPt = new TH1F("histPt", "histPt", 100, 0.1, 2); 
+   TH1F *histMass = new TH1F("histMass", "histMass", 100, 1, 5);
    TH1F *histRapidity = new TH1F("histRapidity", "histRapidity", 100, -3, 3);
  
    // Variable to store muon mass
    const float muMass = 0.1057; // GeV
-   
+   const float elecMass = 0.00051;
+ 
    // variable to check how many counts of mass between 2.9 and 3.2 GeV
    int massCount = 0;
 
@@ -37,12 +38,14 @@ void AnalysisData23::Loop()
    int new_track_TRTHits;
    float new_track_PixelEdX;
    int areElec;
+   int areMu;
 
    // Create new Branches
    newtree->Branch("track_PixelHits", &new_track_PixelHits, "track_PixelHits/I");
    newtree->Branch("track_TRTHits", &new_track_TRTHits, "track_TRTHits/I");
    newtree->Branch("track_PixelEdX", &new_track_PixelEdX, "track_PixelEdX/F");
    newtree->Branch("areElec", &areElec, "areElec/I");   
+   newtree->Branch("areMu", &areMu, "areMu/I");   
 
 
    Long64_t nbytes = 0, nb = 0;
@@ -58,26 +61,38 @@ void AnalysisData23::Loop()
 
 	    // 2. Define the two vectors
             TLorentzVector v0, v1, vSystem;
-
+	   
             // Check Kinematic cuts (e.g. Pt > 0.1, Eta < 2.5) for BOTH tracks
             bool passKinematics = true;
-            if (track_pt->at(0) <= 0.1 || abs(track_eta->at(0)) >= 2.5) passKinematics = false;
-            if (track_pt->at(1) <= 0.1 || abs(track_eta->at(1)) >= 2.5) passKinematics = false;
-
-            if (passKinematics) {
-                // Set Vectors
+            if (track_pt->at(0) <= 0.1 || abs(track_eta->at(0)) >= 2) passKinematics = false;
+            if (track_pt->at(1) <= 0.1 || abs(track_eta->at(1)) >= 2) passKinematics = false;
+            
+	    if (passKinematics) {
+   	    
+	    // isSignal to check if there are e/mu to reconstruct and not a background
+	    bool isSignal = false;
+	    // Logic to check if electrons  
+     	    if (nElec >= 1) {
+		areElec = 1;
+		areMu = 0;
+                isSignal = true;
+		// Set Vectors as electrons
+                v0.SetPtEtaPhiM(track_pt->at(0), track_eta->at(0), track_phi->at(0), elecMass);
+                v1.SetPtEtaPhiM(track_pt->at(1), track_eta->at(1), track_phi->at(1), elecMass);
+     	    } else if(nMuon >=1){
+		areElec = 0;
+                areMu = 1;
+		isSignal = true;
+		// Set Vectors
                 v0.SetPtEtaPhiM(track_pt->at(0), track_eta->at(0), track_phi->at(0), muMass);
                 v1.SetPtEtaPhiM(track_pt->at(1), track_eta->at(1), track_phi->at(1), muMass);
-
-                // Reconstruct the system
+	    }
+            if (isSignal){
+		// Reconstruct the system
                 vSystem = v0 + v1;
+	    // DeltaR to check if products are from one collisions
+	    if (v0.DeltaR(v1) >= 0.1){
 
-   // Logic to check if electrons  
-     if (nElec >= 1) {
-	areElec = 1;
-     } else {
-	areElec = 0;
-     }
                 if(vSystem.Perp() < 0.2 && vSystem.M() > 2.9 && vSystem.M() < 3.2){
 		  new_track_PixelHits = track_PixelHits->at(0);
 		  new_track_TRTHits = track_TRTHits->at(0);
@@ -94,9 +109,11 @@ void AnalysisData23::Loop()
 		histRapidity->Fill(vSystem.Rapidity());
 
                 // Count specific mass range instances
-                    massCount++;
+                    //massCount++;
                 }
-            }
+	     }
+             } 
+	   }
         }
 }   
 newtree->Write();
