812a9f5cea06d1f8cd6662aebb5673989a34d0bf
macbla12 <mblaut@student.agh.edu.pl>
2025-11-29 14:54:45 +0100

    Add files via upload

--- Analysis.C
+++ Analysis.C
@@ -13,7 +13,7 @@ void Analysis::Loop()
 
    float PixelHits;
    float PixelTRTHits;
-   float PixeldEdX;
+   float PixelSCTHits;
    bool  IsMuon;
 
    TFile *file = new TFile("MLData.root", "RECREATE");
@@ -21,13 +21,13 @@ void Analysis::Loop()
 
    MLDataTree->Branch("track_PixelHits", &PixelHits, "track_PixelHits/F");
    MLDataTree->Branch("track_TRTHits", &PixelTRTHits, "track_PixelTRTHits/F");
-   MLDataTree->Branch("track_PixeldEdX", &PixeldEdX, "track_PixeldEdX/F");
+   MLDataTree->Branch("track_SCTHits", &PixelSCTHits, "track_PixelSCTHits/F");
    MLDataTree->Branch("IsMuon", &IsMuon, "IsMuon/B");
 
 
    Long64_t nentries = fChain->GetEntriesFast();
    TH1D *Number = new TH1D("Number","Number",15,-0.5,14.5);
-   TH1D *HRadEtaPhi = new TH1D("RadEtaPhi","RadEtaPhi",100,-0.01,0.1);
+   TH1D *HRadEtaPhi = new TH1D("RadEtaPhi","RadEtaPhi",100,-0.01,2);
 
 
    TH1D *DiMass = new TH1D("dimass","dimass",50,2.9,3.2);
@@ -37,12 +37,12 @@ void Analysis::Loop()
    TH1D *ElectronEta =new TH1D("ElectronEta","ElectronEta",50,-3,3);
    TH1D *ElectronEnergy =new TH1D("ElectronEnergy","ElectronEnergy",50,1,6);
    TH1D *ElectronPt =new TH1D("ElectronPt","ElectronPt",50,0.5,1.8);
-   TH1D *ElectronPhi =new TH1D("ElectronPhi","ElectronPhi",50,-pi-0.1,pi+0.1);
+   TH1D *ElectronPhi =new TH1D("ElectronPhi","ElectronPhi",30,-pi-0.1,pi+0.1);
 
    TH1D *MuonEta =new TH1D("MuonEta","MuonEta",50,-3,3);
    TH1D *MuonEnergy =new TH1D("MuonEnergy","MuonEnergy",50,1,6);
    TH1D *MuonPt =new TH1D("MuonPt","MuonPt",50,0.5,1.8);
-   TH1D *MuonPhi =new TH1D("MuonPhi","MuonPhi",50,-pi-0.1,pi+0.1);
+   TH1D *MuonPhi =new TH1D("MuonPhi","MuonPhi",30,-pi-0.1,pi+0.1);
    
 
    Long64_t nbytes = 0, nb = 0;
@@ -50,7 +50,6 @@ void Analysis::Loop()
    //for (Long64_t jentry=0; jentry<1000;jentry++) {
       if(jentry%10000==0) cout<<"Processing "<<jentry<<" event..."<<endl;
       int count=0;
-      float ElEta=0,ElPhi=0;
       TLorentzVector Electron;
       vector<TLorentzVector> Muon(2);
       vector<TLorentzVector> t(2);
@@ -58,12 +57,8 @@ void Analysis::Loop()
       if (ientry < 0) break;
       nb = fChain->GetEntry(jentry);   nbytes += nb;
 
-      Number->Fill(nElec);
-      if(nElec==2)
-      {
-         ElEta=electron_eta->at(0);
-         ElPhi=electron_phi->at(0);
-      }
+      
+     
       
       
       if(track_n==2)
@@ -92,16 +87,58 @@ void Analysis::Loop()
                Rapidity->Fill(dipartic.Rapidity());
                DiMass->Fill((dipartic).M());
 
-               vector<double> RadEtaPhi(2);
+               double RadEtaPhi;
 
-               if((dipartic).M()>3.05)
+               int c=0;
+               
+               for(int i=0;i<eg_cluster_n;i++)
+               {
+                  RadEtaPhi=sqrt(pow(t[0].Eta()-eg_cluster_eta->at(i),2)+pow(t[0].Phi()-eg_cluster_phi->at(i),2));
+                  double temp;
+                  HRadEtaPhi->Fill(RadEtaPhi);
+                  if(RadEtaPhi<0.5)
+                  {
+                     
+                     if(c==0)
+                     {
+                        Electron=t[1];
+                        temp=RadEtaPhi;
+                     } 
+                     if(c>0 && temp>RadEtaPhi)
+                     {
+                        Electron=t[1];
+                        
+                        temp=RadEtaPhi;
+                     }
+                     
+                     c++;
+                  } 
+               }
+               
+               Number->Fill(c);
+               //if(c>1) cout<<"Error "<<c<<"  "<<jentry<<endl;
+               
+               if(Electron.E()!=0)
+               {
+                  PixelHits=track_PixelHits->at(1);
+                  PixelTRTHits=track_TRTHits->at(1);
+                  PixelSCTHits=track_SCTHits->at(1);
+                  IsMuon=0;
+                  MLDataTree->Fill();
+
+                  ElectronEnergy->Fill(Electron.E());
+                  ElectronEta->Fill(Electron.Eta());
+                  ElectronPt->Fill(Electron.Pt());
+                  ElectronPhi->Fill(Electron.Phi());
+               }
+               if((dipartic).M()>3 && Electron.E()==0)
                {
                   for(int i=0;i<2;i++)
                   {
                      Muon[i]=t[i];
                      PixelHits=track_PixelHits->at(i);
                      PixelTRTHits=track_TRTHits->at(i);
-                     PixeldEdX=track_PixeldEdX->at(i);
+                     PixelSCTHits=track_SCTHits->at(i);
                      IsMuon=1;
                      MLDataTree->Fill();
                      MuonEnergy->Fill(Muon[i].E());
@@ -109,38 +146,7 @@ void Analysis::Loop()
                      MuonPt->Fill(Muon[i].Pt());
                      MuonPhi->Fill(Muon[i].Phi());
                   }
-               }
-               else
-               {
-                  for(int i=0;i<2;i++) RadEtaPhi[i]=sqrt(pow(t[i].Eta()-ElEta,2)+pow(t[i].Phi()-ElPhi,2));
-                  int index;
-                  if(RadEtaPhi[0]<RadEtaPhi[1])
-                  {
-                     if(RadEtaPhi[0]<0.1) Electron=t[1];
-                     HRadEtaPhi->Fill(RadEtaPhi[0]);
-                     index=1;
-                  }
-                  else
-                  {
-                     if(RadEtaPhi[1]<0.1) Electron=t[0];
-                     HRadEtaPhi->Fill(RadEtaPhi[1]);
-                     index=0;
-                  } 
-                  if(Electron.E()!=0)
-                  {
-                     PixelHits=track_PixelHits->at(index);
-                     PixelTRTHits=track_TRTHits->at(index);
-                     PixeldEdX=track_PixeldEdX->at(index);
-                     IsMuon=0;
-                     MLDataTree->Fill();
-
-                     ElectronEnergy->Fill(Electron.E());
-                     ElectronEta->Fill(Electron.Eta());
-                     ElectronPt->Fill(Electron.Pt());
-                     ElectronPhi->Fill(Electron.Phi());
-                  }
-               }
-               
+               }              
             }
 
          }
@@ -180,6 +186,7 @@ void Analysis::Loop()
    c1.cd(3);
    ElectronPt->Draw();
    c1.cd(4);
+   ElectronPhi->SetMinimum(0);
    ElectronPhi->Draw();
    c1.SaveAs("Plots/test.pdf");
 
@@ -192,8 +199,10 @@ void Analysis::Loop()
    c1.cd(3);
    MuonPt->Draw();
    c1.cd(4);
+   MuonPhi->SetMinimum(0);
    MuonPhi->Draw();
    c1.SaveAs("Plots/test.pdf");
+
 
    c1.SaveAs("Plots/test.pdf]");
    
