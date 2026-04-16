295d811a6625ba35381c24d7b0ae3ab63b7e6bd9
macbla12 <mblaut@student.agh.edu.pl>
2026-04-16 16:03:12 +0200

    ONNX

--- PyGymRF.py
+++ PyGymRF.py
@@ -75,7 +75,7 @@ rf.fit(X_train, y_train)
 initial_type = [('float_input', FloatTensorType([None, 9]))]
 onnx_model = convert_sklearn(rf, initial_types=initial_type, target_opset=17, options={type(rf): {"zipmap": False}})
 onnx_model.ir_version = 9
-with open("Data/RFMuonElectron.onnx", "wb") as fo:
+with open("ONNX/RFMuonElectron.onnx", "wb") as fo:
     fo.write(onnx_model.SerializeToString())
 
 # ========================
